4210c421b467806cbd147a20865b963047c2a23e
Lllewisreynolds <lsreynolds108@gmail.com>
2023-12-13 21:42:18 +0000

    Post request created to send new note data to server, with each new note ascribed a unique identifier

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -8,3 +8,27 @@ const { v4 as uuidv4 } = require('uuid');
 const { notes } = require('../../db/db');
 // Importing functions from module responsible for handling logic around note creation and retrieval
 const { newNoteCreation, fetchNote } = require('../../lib/notes-logic');
+
+// Defining route handler function - retrieving notes data from db and sending back to client as JSON object
+router.get('/notes', (req, res) => {
+    res.json(notes);
+});
+
+// Asynchronous route handler function
+router.post('/notes', async (req, res) => {
+    // Generating a unique ID for the new note using function imported from uuid module
+    const id = uuidv4();
+    // Spread operator used to merge unique ID generation with the request body
+    const newNote = { id, ...req.body };
+
+    /* 'await' used to ensure that the createNewNote function has completed its task before continuing with sending the JSON response. 
+    This ensures that the response is only sent when the note is successfully created */
+    await createNewNote(newNote, notes);
+
+    // Send the created note as a JSON response
+    res.json(newNote);
+});
+
+// plan is to use imported fetchNote() function to allow for note deletion - will return to later
+
+module.exports = router;
