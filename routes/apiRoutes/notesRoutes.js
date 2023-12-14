7331054897f06a89ad2185ab61db0a2d9b956edb
Lllewisreynolds <lsreynolds108@gmail.com>
2023-12-14 10:58:22 +0000

    testing of unique id module showed that wrong  library was installed. Comments in database also caused code to throw error as JSON object will not allo w this

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -3,7 +3,7 @@ Defines our routing infrastructure, directing and handling any incoming HTTP req
 const router = require('express').Router();
 /*UUID module imported. Library will be used to generate unique identifiers for each user note
 Importing version 4 method specifically - other versions - 1 & 5 - do not provide complete anonymity or randomness */
-const { v4 as uuidv4 } = require('uuid');
+const uuidv4 = require('uuid').v4;
 // Importing notes from database
 const { notes } = require('../../db/db');
 // Importing functions from module responsible for handling logic around note creation and retrieval
